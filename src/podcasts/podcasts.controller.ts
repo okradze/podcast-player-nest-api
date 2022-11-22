8cb97e82e1ac5609a471294f6f516a38d537e745
Mirian Okradze <okradzemirian@gmail.com>
2022-11-22 20:01:59 +0400

    feat: define podcasts controller endpoints

--- src/podcasts/podcasts.controller.ts
+++ src/podcasts/podcasts.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 
 @Controller('podcasts')
-export class PodcastsController {}
+export class PodcastsController {
+  @Get('best')
+  getBestPodcasts() {
+    return 'best'
+  }
+
+  @Get('curated')
+  getCurated() {
+    return 'curated'
+  }
+
+  @Get(':id')
+  getPodcast(@Param('id') id: string) {
+    return `podcast id: ${id}`
+  }
+
+  @Get(':id/recommendations')
+  getPodcastRecommendations(@Param('id') id: string) {
+    return `podcast recommendations for id: ${id}`
+  }
+}
