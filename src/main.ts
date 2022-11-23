141e03b9e29816183ac6c804e822827c90631999
Mirian Okradze <okradzemirian@gmail.com>
2022-11-23 17:29:47 +0400

    feat: setup validation pipe and validate signup fields

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.enableCors({
     origin: 'http://localhost:3000',
   })
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
   await app.listen(8000)
 }
 bootstrap()
--- src/modules/auth/dto/signup-dto.ts
+++ src/modules/auth/dto/signup-dto.ts
@@ -1,5 +1,12 @@
-export interface SignupDto {
+import { IsEmail, Length } from 'class-validator'
+
+export class SignupDto {
+  @Length(2, 255)
   fullName: string
+
+  @IsEmail()
   email: string
+
+  @Length(8, 255)
   password: string
 }
