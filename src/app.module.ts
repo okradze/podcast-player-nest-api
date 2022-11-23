56b02fd98581f75374dc3c5c17f49c911efbb00e
Mirian Okradze <okradzemirian@gmail.com>
2022-11-23 11:15:33 +0400

    fix: db port to number

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { User } from './users/user.model'
       useFactory: (configService: ConfigService) => ({
         dialect: 'postgres',
         host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
+        port: +configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_DATABASE_NAME'),
