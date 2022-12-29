3b5d21a57a81c24db188985e21ef5385c64fead3
Mirian Okradze <okradzemirian@gmail.com>
2022-12-29 12:29:10 +0400

    feat: add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { SequelizeModule } from '@nestjs/sequelize'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './modules/auth/auth.module'
 import { PodcastsModule } from './modules/podcasts/podcasts.module'
 import { UsersModule } from './modules/users/users.module'
 import { User } from './modules/users/user.model'
+import { APP_GUARD } from '@nestjs/core'
 
 @Module({
   imports: [
@@ -25,11 +27,21 @@ import { User } from './modules/users/user.model'
       }),
       inject: [ConfigService],
     }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 60,
+    }),
     AuthModule,
     UsersModule,
     PodcastsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
