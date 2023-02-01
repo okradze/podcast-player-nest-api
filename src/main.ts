d6162dae56570fcdeb3793047acbdf6a2bfd98e5
Mirian Okradze <okradzemirian@gmail.com>
2023-02-01 09:54:50 +0400

    chore: fix server port

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
-  await app.listen(8000)
+  await app.listen(process.env.PORT || 8000)
 }
 bootstrap()
